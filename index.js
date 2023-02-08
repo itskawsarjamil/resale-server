c41f8b4d063363c08bc28474b869ef16f49903e9
Kawsar Jamil <68264633+itskawsarjamil@users.noreply.github.com>
2023-02-09 01:18:08 +0600

    payment api build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-
+const stripe = require("stripe")(process.env.stripe_key);
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -29,6 +29,40 @@ const categoriesCollection = client.db("resale").collection("categories");
 const booksCollection = client.db("resale").collection("books");
 const usersCollection = client.db("resale").collection("users");
 const ordersCollection = client.db("resale").collection("orders");
+const paymentsCollection = client.db('resale').collection('payments');
+
+app.post('/create-payment-intent', async (req, res) => {
+    const order = req.body;
+    const price = order.bookPrice;
+    const amount = price * 100;
+    // console.log(amount);
+    const paymentIntent = await stripe.paymentIntents.create({
+        currency: 'usd',
+        amount: amount,
+        "payment_method_types": [
+            "card"
+        ]
+    });
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
+
+app.post('/payments', async (req, res) => {
+    const payment = req.body;
+    const result = await paymentsCollection.insertOne(payment);
+    const id = payment.orderId;
+    const filter = { _id: new ObjectId(id) };
+    const updatedDoc = {
+        $set: {
+            paid: true,
+            transactionId: payment.transactionId
+        }
+    }
+    const updatedResult = await ordersCollection.updateOne(filter, updatedDoc)
+    res.send(result);
+})
+
 
 app.post('/users', async (req, res) => {
     try {
@@ -236,7 +270,38 @@ app.get("/sellers", async (req, res) => {
     }
 })
 
+app.put("/sellerverify", async (req, res) => {
+    try {
+        const mail = req.query.mail;
+        const filter = { email: mail };
+        console.log(filter);
+        // const data = await usersCollection.findOne(query);
+        const options = { updsert: true }
+        const updateDoc = {
+            $set: {
+                appVerified: true
+            },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc, options);
+        // console.log(result);
+        res.send(result);
 
+    }
+    catch (e) {
+        console.log(e);
+        res.send({
+            "status": false,
+            "error": e
+        })
+    }
+})
+app.get('/order/:id', async (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const query = { _id: new ObjectId(id) };
+    const order = await ordersCollection.findOne(query);
+    res.send(order);
+})
 
 app.listen(port, () => {
     console.log(`resale server is running on port: ${port}`);
