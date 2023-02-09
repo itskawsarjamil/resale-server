76e690fc886f3a66f7bcfec90a98af5f7bab32b3
Kawsar Jamil <68264633+itskawsarjamil@users.noreply.github.com>
2023-02-10 01:15:48 +0600

    wishlist and advertise api complete

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ const booksCollection = client.db("resale").collection("books");
 const usersCollection = client.db("resale").collection("users");
 const ordersCollection = client.db("resale").collection("orders");
 const paymentsCollection = client.db('resale').collection('payments');
+const advCollection = client.db('resale').collection('adv');
+const wishlistCollection = client.db('resale').collection('wishlist');
+
+
 
 app.post('/create-payment-intent', async (req, res) => {
     const order = req.body;
@@ -50,6 +54,16 @@ app.post('/create-payment-intent', async (req, res) => {
 
 app.post('/payments', async (req, res) => {
     const payment = req.body;
+
+    //delete from advertise
+    const orderId = payment.orderId;
+    const findOrderquery = { _id: new ObjectId(orderId) };
+    const findOrderData = await ordersCollection.findOne(findOrderquery);
+    const productId = findOrderData.product_id;
+    const advQuery = { _id: new ObjectId(productId) };
+    const deletedresult = await advCollection.deleteOne(advQuery);
+
+    //insert in payment collection
     const result = await paymentsCollection.insertOne(payment);
     const id = payment.orderId;
     const filter = { _id: new ObjectId(id) };
@@ -110,6 +124,61 @@ app.post('/g-users', async (req, res) => {
     }
 })
 
+app.get('/iswhat/:email', async (req, res) => {
+    try {
+        const mail = req.params.email;
+        const query = { email: mail };
+        const result = await usersCollection.findOne(query);
+        // console.log(result);
+        return res.send(result);
+    }
+    catch (e) {
+        console.log(e);
+        res.send({
+            "status": false,
+            "error": e
+        })
+    }
+})
+app.get('/isseller/:email', async (req, res) => {
+    try {
+        const mail = req.params.email;
+        const query = { email: mail };
+        const result = await usersCollection.findOne(query);
+        // console.log(result);
+        if (result.role === 'seller') {
+            return res.send({ isSeller: true });
+        }
+        return res.send({ isSeller: false });
+    }
+    catch (e) {
+        console.log(e);
+        res.send({
+            "status": false,
+            "error": e
+        })
+    }
+})
+app.get('/isadmin/:email', async (req, res) => {
+    try {
+        const mail = req.params.email;
+        const query = { email: mail };
+        const result = await usersCollection.findOne(query);
+        // console.log(result);
+        if (result.role === 'admin') {
+            return res.send({ isAdmin: true });
+        }
+        return res.send({ isAdmin: false });
+    }
+    catch (e) {
+        console.log(e);
+        res.send({
+            "status": false,
+            "error": e
+        })
+    }
+})
+
 app.get('/Categories', async (req, res) => {
     try {
         const query = {};
@@ -128,7 +197,10 @@ app.get('/Categories', async (req, res) => {
 app.get('/CategoryBooks/:id', async (req, res) => {
     try {
         const id = req.params.id;
-        const query = { category_id: id };
+        const query = {
+            category_id: id,
+            sold: false
+        };
         const result = await booksCollection.find(query).toArray();
         res.send(result);
     }
@@ -274,7 +346,7 @@ app.put("/sellerverify", async (req, res) => {
     try {
         const mail = req.query.mail;
         const filter = { email: mail };
-        console.log(filter);
+        // console.log(filter);
         // const data = await usersCollection.findOne(query);
         const options = { updsert: true }
         const updateDoc = {
@@ -321,9 +393,66 @@ app.delete("/deletebook/:id", async (req, res) => {
     try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
-        // console.log(query);
-        const result = await booksCollection.deleteOne(query);
-        // console.log(result);
+        console.log(query);
+        const adfind = { product_id: id }
+        const addresult = await advCollection.deleteOne(adfind);
+        const dresult = await booksCollection.deleteOne(query);
+        console.log(addresult, dresult);
+        res.send(dresult);
+    }
+    catch (e) {
+        console.log(e);
+        res.send({
+            "status": false,
+            "error": e
+        })
+    }
+})
+app.get('/wishlist', async (req, res) => {
+    try {
+        // console.log("inside of wishlist");
+        const query = {};
+        const result = await wishlistCollection.find(query).toArray();
+        return res.send(result);
+    }
+    catch (e) {
+        console.log(e);
+        res.send({
+            "status": false,
+            "error": e
+        })
+    }
+})
+app.post('/wishlist/:id', async (req, res) => {
+    try {
+        const tempdata = {
+            product_id: req.params.id
+        }
+        const findquery = await wishlistCollection.findOne(tempdata);
+        if (!findquery) {
+            const query = { _id: new ObjectId(req.params.id) }
+            const data = await booksCollection.findOne(query);
+            const result = await wishlistCollection.insertOne(data);
+            return res.send(result);
+            console.log(result);
+        }
+        return res.send({
+            acknowledged: false
+        })
+    }
+    catch (e) {
+        console.log(e);
+        res.send({
+            "status": false,
+            "error": e
+        })
+    }
+})
+
+app.get('/adv', async (req, res) => {
+    try {
+        const query = {};
+        const result = await advCollection.find(query).toArray();
         res.send(result);
     }
     catch (e) {
@@ -335,18 +464,68 @@ app.delete("/deletebook/:id", async (req, res) => {
     }
 })
 
-// app.get('/adv', async (req, res) => {
+app.post('/adv/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const updateDoc = {
+            $set: {
+                adv: true,
+                product_id: id
+            }
+        }
+        const options = { upsert: true };
+        const updateData = await booksCollection.updateOne(query, updateDoc, options);
+        const findData = await booksCollection.findOne(query);
+        // console.log(findData);
+        const result = await advCollection.insertOne(findData);
+        return res.send(result);
+    }
+    catch (e) {
+        console.log(e);
+        res.send({
+            "status": false,
+            "error": e
+        })
+    }
+})
+app.post('/offadv/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const updateDoc = {
+            $set: {
+                adv: false
+            }
+        }
+        const updateData = await booksCollection.updateOne(query, updateDoc);
+        const delquery = { product_id: id };
+        const result = await advCollection.deleteOne(delquery);
+        // console.log(result);
+        return res.send(result);
+    }
+    catch (e) {
+        console.log(e);
+        res.send({
+            "status": false,
+            "error": e
+        })
+    }
+})
+
+// app.get('/sold', async (req, res) => {
 //     const query = {};
 //     const options = { upsert: true };
 //     const updateDoc = {
 //         $set: {
-//             adv: false
+//             sold: false
 //         },
 //     };
 //     const result = await booksCollection.updateMany(query, updateDoc, options);
 //     console.log(result);
 //     res.send(result);
 // })
+
 
 app.listen(port, () => {
     console.log(`resale server is running on port: ${port}`);
